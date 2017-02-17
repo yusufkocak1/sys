a80491afc7cbec96ad29b1469dbd2edc8fd529b7
yusufkocak1 <yusufkocak.53@yandex.com>
2017-02-18 00:33:29 +0300

    activity adding
    SiparisTakip activity adding by yusuf koçak

---
+++ app/src/main/java/yube/com/siparisyonetimsistemi/SiparisTakip.java
@@ -0,0 +1,13 @@
+package yube.com.siparisyonetimsistemi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SiparisTakip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siparis_takip);
+    }
+}
