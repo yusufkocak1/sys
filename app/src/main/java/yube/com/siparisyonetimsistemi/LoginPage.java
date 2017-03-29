d47c56e7e5e1a6b49b1d51923265b00dc57a9e38
yusufkocak1 <yusufkocak.53@yandex.com>
2017-03-29 16:33:31 +0300

    animasyon eklendi
    geçiş animasyonu eklendi alert dialog sorunu çözülemedi

--- app/src/main/java/yube/com/siparisyonetimsistemi/LoginPage.java
+++ app/src/main/java/yube/com/siparisyonetimsistemi/LoginPage.java
@@ -2,6 +2,8 @@ package yube.com.siparisyonetimsistemi;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,77 +11,82 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class LoginPage extends Activity {
 
+    private String[] myDataset={"kebap","döner","ayran"};
 
-
+    private static final String MY_PREFS_NAME = "login";
     //design variable
-    EditText user,password;
+    EditText user, password;
     Button login;
+    private Activity activity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
         //Full screen
+        final SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
 
 
         getWindow().getDecorView().setSystemUiVisibility(
-                          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // Top system menu
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         //design element adding method call
-         designElementadd();
+        designElementadd();
+
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
-               connect();
+                //  connect();
+                editor.putString("user", user.getText().toString());
+                editor.commit();
 
 
-//               if( user.getText().toString().equals("yusuf") && password.getText().toString().equals("yusuf326")){
-//                    Intent cagir=new Intent("yube.com.siparisyonetimsistemi.TABLE");
-//                    startActivity(cagir);
-//               }else if(user.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-//                   Intent adminCagir=new Intent("yube.com.siparisyonetimsistemi.ADMINPANEL");
-//                   startActivity(adminCagir);
-//               }
-//               else if(user.getText().toString().equals("Bx3fn") && password.getText().toString().equals("0325")){
-//                   Intent adminCagir=new Intent(LoginPage.this,AdminPanel.class);
-//                   startActivity(adminCagir);
-//               }
-//
-//                else{
-//                   ViewDialog alert = new ViewDialog();
-//                   alert.showDialog(LoginPage.this, "Şifre yanlış");
-//               }
-//
-           }
-   });
+                if (user.getText().toString().equals("yusuf") && password.getText().toString().equals("yusuf326")) {
+
+                    Intent cagir = new Intent("yube.com.siparisyonetimsistemi.TABLE");
+                    startActivity(cagir);
+                } else if (user.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Intent adminCagir = new Intent("yube.com.siparisyonetimsistemi.ADMINPANEL");
+                    startActivity(adminCagir);
+                } else if (user.getText().toString().equals("Bx3fn") && password.getText().toString().equals("0325")) {
+                    Intent adminCagir = new Intent(LoginPage.this, AdminPanel.class);
+                    startActivity(adminCagir);
+                } else {
+                    ViewDialogL alert = new ViewDialogL();
+                    alert.showDialog(LoginPage.this, "Şifre yanlış");
+                }
+
+            }
+        });
 
 
     }
-
 
 
     private void designElementadd() {
-        user=(EditText) findViewById(R.id.username);
-        password=(EditText) findViewById(R.id.pass);
-        login=(Button) findViewById(R.id.login);
+        user = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.pass);
+        login = (Button) findViewById(R.id.login);
 
 
     }
-    private void connect(){
+
+
+
+
+   /* private void connect(){
         HttpURLConnection connection=null;
         BufferedReader reader=null;
         try {
@@ -111,9 +118,9 @@ public class LoginPage extends Activity {
                 }
         }
 
-    }
+    }*/
 }
-class ViewDialog {
+class ViewDialogL {
 
     public void showDialog(Activity activity, String msg){
         final Dialog dialog = new Dialog(activity);
--- app/src/main/java/yube/com/siparisyonetimsistemi/TablePage.java
+++ app/src/main/java/yube/com/siparisyonetimsistemi/TablePage.java
@@ -1,32 +1,158 @@
 package yube.com.siparisyonetimsistemi;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TablePage extends AppCompatActivity {
+public class TablePage extends AppCompatActivity implements View.OnClickListener {
+    private static final String MY_PREFS_NAME ="login" ;
+    Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10;
 
+    ListView listemiz=null;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+
+    String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.table_page);
-        //Full screen
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                       // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // Top system menu
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        //design element adding method call
         designElementadd();
+        //Full screen
+
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("text", null);
+
+            username = prefs.getString("user", "No name defined");//"No name defined" is the default value.
+        String[] ulkeler =
+                {"Türkiye", "Almanya", "Avusturya", "Amerika","İngiltere",
+                        "Macaristan", "Yunanistan", "Rusya", "Suriye", "İran", "Irak",
+                        "Şili", "Brezilya", "Japonya", "Portekiz", "İspanya",
+                        "Makedonya", "Ukrayna", "İsviçre"};
+        //(A) adımı
+        listemiz=new ListView(this);
+
+        //(B) adımı
+        ArrayAdapter<String> veriAdaptoru=new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, android.R.id.text1, ulkeler);
+
+        //(C) adımı
+        listemiz.setAdapter(veriAdaptoru);
+
+        listemiz.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ViewGroup vg=(ViewGroup) view;
+                TextView txt=(TextView) findViewById(R.id.txtitem);
+                Toast.makeText(TablePage.this,txt.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        // Perform action on click
+        switch (v.getId()) {
+            case R.id.button: {
+
+                showDialog(TablePage.this, "masa no:" + 1);
+
+                break;
+            }
+            case R.id.button2: {
+                showDialog(TablePage.this, "masa no:2");
+                break;
+            }
+            case R.id.button3: {
+                showDialog(TablePage.this, "" + R.id.button3);
+                break;
+            }
+            case R.id.button4: {
+                showDialog(TablePage.this, "" + R.id.button4);
+                break;
+            }
+            case R.id.button5: {
+
+                break;
+            }
+            case R.id.button6: {
+
+                break;
+            }
+            case R.id.button7: {
+
+                break;
+            }
+            case R.id.button9: {
+
+                break;
+            }
+            case R.id.button10: {
+
+                break;
+            }
+            case R.id.button11: {
+
+                break;
+            }
+
+
+        }
     }
 
     private void designElementadd() {
-        
+        bt1 = (Button) findViewById(R.id.button);
+        bt2 = (Button) findViewById(R.id.button2);
+        bt3 = (Button) findViewById(R.id.button3);
+        bt4 = (Button) findViewById(R.id.button4);
+        bt5 = (Button) findViewById(R.id.button5);
+        bt6 = (Button) findViewById(R.id.button6);
+        bt7 = (Button) findViewById(R.id.button7);
+        bt8 = (Button) findViewById(R.id.button11);
+        bt9 = (Button) findViewById(R.id.button9);
+        bt10 = (Button) findViewById(R.id.button10);
+        // table_no=(TextView) findViewById(R.id.table_no);
+
+
+        bt1.setOnClickListener(this);
+        bt2.setOnClickListener(this);
+        bt3.setOnClickListener(this);
+        bt4.setOnClickListener(this);
+        bt5.setOnClickListener(this);
+        bt6.setOnClickListener(this);
+        bt7.setOnClickListener(this);
+        bt8.setOnClickListener(this);
+        bt9.setOnClickListener(this);
+        bt10.setOnClickListener(this);
 
 
     }
@@ -51,12 +177,48 @@ public class TablePage extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if (id== R.id.back){
-            Intent cagir= new Intent(this, LoginPage.class);
+        } else if (id == R.id.back) {
+            Intent cagir = new Intent(this, LoginPage.class);
             startActivity(cagir);
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void showDialog(Activity activity, String msg) {
+
+
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(TablePage.this);
+        builder.setCancelable(true);
+
+        ListView liste=(ListView) findViewById(R.id.listview);
+        liste=listemiz;
+builder.setView(R.layout.table_alert_dialog);
+        final Dialog dialog =builder.create();
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
+      // dialog.setContentView(R.layout.table_alert_dialog);
+
+        TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+        TextView user = (TextView) dialog.findViewById(R.id.usern);
+        user.setText(username);
+        text.setText(msg);
+        Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+      });
+
+        dialog.show();
+
+    }
+
+
+
 }
